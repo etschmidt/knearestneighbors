2bed4894b8b6a261ab74a282ad7061f73ece0370
Ethan Windows <ethan.t.schmidt@gmail.com>
2018-08-14 18:39:57 -0400

    asdf

--- wine.py
+++ wine.py
@@ -1,5 +1,10 @@
 # https://www.datacamp.com/community/tutorials/k-nearest-neighbor-classification-scikit-learn
 # KNN with multiple labels
+# this calculates Euclidean distance
+# but is sensitive to magnitudes;
+# data must be scaled accordingly
+
+
 # get the wine data set
 
 from sklearn import datasets
@@ -25,4 +30,23 @@ print(df)
 # split the data into train/test sets
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(wine.data, wine.target, test_size=0.3) # 70% training and 30% test
+#test size can also be random
+X_train, X_test, y_train, y_test = train_test_split(wine.data, wine.target,
+ test_size=0.3) # 70% training and 30% test
+
+#create the classifier
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=5)
+
+#train the set
+knn.fit(X_train, y_train)
+
+# amke the prediction
+y_prediction = knn.predict(X_test)
+# print(y_prediction)
+
+#judge the accuracy
+from sklearn import metrics
+
+print('Accuracy: ', metrics.accuracy_score(y_test, y_prediction))
