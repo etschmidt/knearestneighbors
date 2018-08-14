24228031ff28d7c33cbca887e9e9e4c31760ecc4
Ethan Windows <ethan.t.schmidt@gmail.com>
2018-08-14 17:54:30 -0400

    asdF

--- knn.py
+++ knn.py
@@ -1,1 +1,26 @@
-knn.py
+# X variables that would determine outcomes
+# First Feature
+weather=['Sunny','Sunny','Overcast','Rainy','Rainy','Rainy','Overcast','Sunny','Sunny',
+'Rainy','Sunny','Overcast','Overcast','Rainy']
+# Second Feature
+temp=['Hot','Hot','Hot','Mild','Cool','Cool','Cool','Mild','Cool','Mild','Mild','Mild','Hot','Mild']
+
+# Y variable
+play=['No','No','Yes','Yes','Yes','No','Yes','No','Yes','Yes','Yes','Yes','Yes','No']
+
+# Import LabelEncoder
+from sklearn import preprocessing
+
+# create the encoder
+le = preprocessing.LabelEncoder()
+
+weather_encoded = le.fit_transform(weather)
+temp_encoded = le.fit_transform(temp)
+play_encoded = le.fit_transform(play)
+
+# print(weather_encoded, temp_encoded, play_encoded)
+
+#combine the data into single set
+#this makes a Euclidean plane
+features = list(zip(weather_encoded, temp_encoded))
+
