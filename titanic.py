f082198f7959791f450f4efbca10bbec6e78fa06
Ethan Windows <ethan.t.schmidt@gmail.com>
2018-08-16 23:56:37 -0400

    asdf

--- titanic.py
+++ titanic.py
@@ -8,7 +8,6 @@
 # Cluster Titanic passengers into Survival categories based on their data
 
 # Dependencies
-
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
@@ -22,5 +21,6 @@ train = pd.read_csv(train_url)
 test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
 test = pd.read_csv(test_url)
 
-print(train.columns)
-print(test.columns)
+print(train.head())
+print("\n")
+print(test.head())
